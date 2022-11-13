1fe9f5fdb3e6ee2bba54456fe6aace2fb9f8682d
Rishabh <56164824+rishabhsdev@users.noreply.github.com>
2022-11-13 11:37:31 +0530

    Added CORS

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const apiRoutes = require('./routes/api_routes');
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 
 app.use(apiRoutes);
 
